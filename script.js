720f516e608d52692c81aeda775dd6e49fb649ee
danielkarlsson2 <daniel@dkmusic.se>
2021-12-29 20:51:29 +0100

    För och efternamn minst två bokstäver för submit

--- script.js
+++ script.js
@@ -1,25 +1,33 @@
 // ----- Variablar -----
 const firstName = document.querySelector('#firstName');
 const lastName = document.querySelector('#lastName');
+const email = document.querySelector('#email');
 
 
 const regForm = document.querySelector('#regForm');
 
-const user = [
-    {firstName},
-    {lastName},
-    {email}
-]
+const output = document.querySelector('#output');
+
 
 
 
 // ----- Validering -----
-const validate = (id) => {
-    const input = document.querySelector(id);
-    const error = document.querySelector(id + '-error');
+const validateFirstName = (fNameInput) => {
+    const input = document.querySelector('#firstName');
+    const error = document.querySelector('#firstName-error');
 
     if(input.value === '') {
         error.innerText = 'Du måste ange ett namn';
+        firstName.classList.add('invalid');        
+    }
+}
+const validateLastName = (lNameInput) => {
+    const input = document.querySelector('#lastName');
+    const error = document.querySelector('#lastName-error');
+
+    if(input.value === '') {
+        error.innerText = 'Du måste ange ett namn';
+        lastName.classList.add('invalid');        
     }
 }
 const validateEmail = (emailInput) => {
@@ -28,19 +36,72 @@ const validateEmail = (emailInput) => {
     
     if(input.value === '') {
         error.innerText = 'Du måste ange en emailadress';
+        email.classList.add('invalid');
     }
 }
 
-// ----- Validering -----
+// ----- Minst två tecken validering -----
+// document.querySelector('#submit').disabled = true;   
+// document.querySelector('#regForm').addEventListener('keyup', e => {
+    
+//     // if (e.target.value == "") {
+//     // if (firstName.value.length < 2 && lastName.value.length < 2) {
+//     if (firstName.value.length < 2) {
+//       document.querySelector('#submit').disabled = true;
+//     }
+//     else if (lastName.value.length < 2) {
+//         document.querySelector('#submit').disabled = true;
+//     }
+//     else {
+//       document.querySelector('#submit').disabled = false;
+//     }
+//   });
 
 
 
-console.log(user);
+
+// regForm.addEventListener()
+
+
+
+// ----- END  -----
+
+
+
+
+
+let user = [];
+
+// let user = [
+//     {firstName: ''},
+//     {lastName: ''},
+//     {email: ''},
+//     {id: ''},
+// ]
+
 
 regForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
-    validate('#firstName');
-    validate('#lastName');
+    user = {
+        firstName: firstName.value,
+        lastName: lastName.value,
+        email: email.value,
+        id: '1'
+    }
+    // submitValid();
+    
+    validateFirstName(); 
+    validateLastName();
     validateEmail();
+    
+    console.log(user);
+
+    
+    output.innerHTML = ` 
+    <div id="firstNameOutput">${firstName.value}  ${lastName.value}</div>
+    <div id="emailOutput">${email.value}</div>`    
+
+    // regForm.reset();
+
 })
