a9194964e5da10cd5f806db8cf6d1247b5b81e59
danielkarlsson2 <daniel@dkmusic.se>
2021-12-30 15:24:57 +0100

    Försöker spara i array och skriva ut

--- script.js
+++ script.js
@@ -66,41 +66,79 @@ const validateEmail = (emailInput) => {
 
 // ----- END  -----
 
+// let user = [];
+let userObjects = [{
+    firstName: '', 
+    lastName: ''},
+    {email:''
+
+}
+    // {firstName: ''},
+    // {lastName: ''},
+    // {email: ''},
+    // {id: ''},
+]
+const createList = () => {
+    for (i = 0; i < userObjects.length; i++) {
+        let listItem = document.createElement('li');
+        listItem.className = 'unstyled';
+        output.appendChild(listItem);
+    }
+}
+
+// let addInfo = function () {
+
+//     const li = document.createElement('li');    
+//     // const info = document.createTextNode(firstName.value + ' ' + lastName.value);
+//     const info = document.createTextNode(userObjects.value);
+
+//     li.appendChild(info);
+//     li.classList.add('unstyled', 'list-group-item', 'list-group-item-info');
+    
+//     const listItem = document.getElementById("output");
+//     listItem.appendChild(li)
+//     // li.appendChild(text);
+//     // output.appendChild(li);
+//     console.log(output);
+//     // li.appendChild(user);
+
+//     // output.innerHTML =  
+//     // `<li id="firstNameOutput" class="list-unstyled">${firstName.value} ${lastName.value}</li>
+//     // <li id="emailOutput" class="list-unstyled">${email.value}</li>`
+
+//     // ` <div id="firstNameOutput">${firstName.value}  ${lastName.value}</div>
+//     // <div id="emailOutput">${email.value}</div>`
+// }
 
 
 
 
-let user = [];
 
-// let user = [
-//     {firstName: ''},
-//     {lastName: ''},
-//     {email: ''},
-//     {id: ''},
-// ]
 
 
 regForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
-    user = {
-        firstName: firstName.value,
-        lastName: lastName.value,
-        email: email.value,
-        id: '1'
-    }
+    // user = {
+    //     firstName: firstName.value,
+    //     lastName: lastName.value,
+    //     email: email.value,
+    //     id: '1'
+    // }
     // submitValid();
     
     validateFirstName(); 
     validateLastName();
     validateEmail();
+    // addInfo();
+    createList();
     
-    console.log(user);
+    console.log(userObjects);
 
     
-    output.innerHTML = ` 
-    <div id="firstNameOutput">${firstName.value}  ${lastName.value}</div>
-    <div id="emailOutput">${email.value}</div>`    
+    // output.innerHTML = ` 
+    // <div id="firstNameOutput">${firstName.value}  ${lastName.value}</div>
+    // <div id="emailOutput">${email.value}</div>`    
 
     // regForm.reset();
 
